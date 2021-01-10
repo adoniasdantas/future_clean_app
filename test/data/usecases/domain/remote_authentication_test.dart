e182db140d7b9dfbc0d61561e337d8db05671110
adoniasdantas <adoniasdantas@gmail.com>
2021-01-10 10:21:01 -0300

    ensure RemoteAuthentication throws UnexpecError on 404

--- test/data/usecases/domain/remote_authentication_test.dart
+++ test/data/usecases/domain/remote_authentication_test.dart
@@ -52,4 +52,16 @@ void main() {
     // asset
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw UnexpectedError if HttpClient returns 404', () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.notFound);
+    // act    
+    final future = sut.auth(params);
+    // asset
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
