7cd7ad880df8083eb4633969802310db77d214a4
adoniasdantas <adoniasdantas@gmail.com>
2021-01-10 10:48:24 -0300

    move api details from domain entity to data model

--- lib/domain/entities/account_entity.dart
+++ lib/domain/entities/account_entity.dart
@@ -1,10 +1,5 @@
-import 'dart:convert';
-
 class AccountEntity {
   final String token;
 
   AccountEntity(this.token);
-
-  factory AccountEntity.fromJson(Map json) =>
-      AccountEntity(json['accessToken']);
 }
